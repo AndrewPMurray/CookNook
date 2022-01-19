5565c792434345136ba9e69eae51cd3f756a1a5f
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-01-18 20:12:30 -0500

    added login form

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const { port } = require('./config')
 
 const app = express();
 
