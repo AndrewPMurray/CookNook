02d3131dbb3c8f405da0bc30af732b8418021cf3
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-01-20 16:53:50 -0500

    converted question form from pop-up

--- routes/questions.js
+++ routes/questions.js
@@ -6,6 +6,12 @@ const { check, validationResult } = require('express-validator');
 const { csrfProtection, asyncHandler } = require('../utils')
 const db = require('../db/models');
 
+router.get('/', csrfProtection, asyncHandler(async(req, res) => {
+    res.render('question-form', {
+        csrfToken: req.csrfToken()
+    });
+}));
+
 router.post('/', csrfProtection, asyncHandler(async(req, res) => {
     
 }))
