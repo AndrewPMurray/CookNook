0570073eb8206c95385ee90deacabccfcc064b40
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-01-20 13:13:04 -0500

    progress on question form

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -18,6 +18,35 @@ window.addEventListener("DOMContentLoaded", (event) => {
         truncateText(answer);
     });
 
+    // Generate form when question button clicked
+    const addQuestionButton = document.getElementById('add-question');
+
+    addQuestionButton.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const overlay = document.createElement('div');
+        const formContainer = document.createElement('div');
+
+        overlay.className = 'overlay';
+        
+        overlay.innerHTML = `
+        <div class="question-form-container">
+            <form action="/questions" method="post">
+                <p>Ask a question here!</p>
+                <label for="content">
+                <input type="textarea" name="content" placeholder="begin your question with what, where, why...">
+            </form>
+        </div>
+        `
+
+        document.body.appendChild(overlay);
+        overlay.addEventListener('click', (e) => {
+            console.log(e.target)
+            overlay.remove();
+        });
+    });
+
     // add'l scripts as needed
+
 
 });
