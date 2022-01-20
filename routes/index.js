b7ca6e6f3ff758cab87aa184b361df62fee60d72
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-01-20 17:03:32 -0500

    fixed logout

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,17 @@ const {Question, User, Answer} = require('../db/models')
 /* GET home page. */
 router.get('/', async (req, res, next) => {
 
+  if (!req.session.auth) {
+    res.redirect('/landing');
+  }
   const questions = await Question.findAll({
   include: User,
   limit: 10
   })
   const userId = req.session.auth.userId
-  console.log(userId)
   const answers = await Answer.findAll()
   const users = await User.findAll();
   res.render('index', {users, userId, questions, answers});
-}); //f
+});
 
 module.exports = router;
