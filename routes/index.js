854e9e8d71d74d7c0e6a87c472dbd01b45ab8ce2
Dane Becker <danebecker790@gmail.com>
2022-01-20 15:51:25 -0600

    login fixed, now recognizes and greets clients

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,16 @@ const {Question, User, Answer} = require('../db/models')
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-  console.log(req.session.auth)
+
   const questions = await Question.findAll({
-  include: User
+  include: User,
+  limit: 10
   })
+  const userId = req.session.auth.userId
+  console.log(userId)
   const answers = await Answer.findAll()
-  res.render('index', {questions, answers});
+  const users = await User.findAll();
+  res.render('index', {users, userId, questions, answers});
 }); //f
 
 module.exports = router;
