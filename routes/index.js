5ff13ed53ecc58c6139680462fccc64556657247
Dane Becker <danebecker790@gmail.com>
2022-01-19 18:52:38 -0600

    skeleton for home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const {Question, User, Answer} = require('../db/models')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {});
+router.get('/', async (req, res, next) => {
+  const questions = await Question.findAll({
+  include: User
+  })
+  questions.forEach(question => {
+    const questionId = question.id
+  })
+  const answers = await Answer.findAll({
+    where: {
+
+    }
+  })
+  res.render('index', {questions});
 });
 
 module.exports = router;
