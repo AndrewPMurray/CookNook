6f4b923eadf64d84b65f15ff896e6fc7921260c3
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-01-21 17:47:46 -0500

    fixed posting comment route

--- routes/comments.js
+++ routes/comments.js
@@ -18,7 +18,7 @@ router.post('/', async (req, res) => {
     const newAnswer = await Answer.create({
         content: comment, userId, questionId
     })
-    return res.redirect(`/question${questionId}`)
+    return res.redirect(`/question/${questionId}`)
 })
 
 router.post('/delete/:id(\\d+)', async(req, res) => {
